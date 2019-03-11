1e9f4281e4bb6dd888a4b6780ec22dc75547ff96
Gurp <kooner@gmail.com>
2019-03-11 20:41:07 +0000

    made a style change

--- requests.js
+++ requests.js
@@ -1,11 +1,8 @@
 var request = require('request');
 var fs = require('fs');
 
-request('https://sytantris.github.io/http-examples/future.jpg', function(err, response, body) {
-  if (err) throw err;
-
-  //console.log('Response Status Code:', body);
-}).on('response', function(res){
+request('https://sytantris.github.io/http-examples/future.jpg')
+.on('response', function(res){
   console.log('Response Status Code:', res.statusMessage);
   console.log(res.headers['content-type']);
   console.log('Downloading image...');
